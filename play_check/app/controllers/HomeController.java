9b503e8200a2d9444d20667f87d385f667d68d65
94ju <janithherath94@gmail.com>
2018-02-20 19:43:25 +0530

    play files second commit

--- play_check/app/controllers/HomeController.java
+++ play_check/app/controllers/HomeController.java
@@ -2,7 +2,9 @@ package controllers;
 
 import play.mvc.*;
 
-import views.html.*;
+
+import views.html.index;
+import views.html.Home.*;
 
 /**
  * This controller contains an action to handle HTTP requests
@@ -17,7 +19,15 @@ public class HomeController extends Controller {
      * <code>GET</code> request with a path of <code>/</code>.
      */
     public Result index() {
-        return ok(index.render("Your new application is ready."));
+
+
+        return ok("Hello i am coming sri lanka");
+    }
+    public Result about(){
+        return ok("sri lanka won the match");
+    }
+    public Result welcome(String name){
+        return ok(welcome.render(name));
     }
 
 }
